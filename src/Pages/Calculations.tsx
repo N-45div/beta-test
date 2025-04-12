ee8938b64b368decfddefcf68bd7d9af7ba39b03
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2025-04-12 14:08:49 +0530

    Update Calculations.tsx

--- src/Pages/Calculations.tsx
+++ src/Pages/Calculations.tsx
@@ -86,7 +86,7 @@ const Calculations = () => {
           : "bg-gradient-to-br from-indigo-50 via-teal-50 to-pink-50"
       }`}
     >
-      <Navbar level={"/Level-Three-Quiz"} questionnaire={"/Questionnaire"} live_generation={"/Live_Generation"} calculations={"/Calculations"}/>
+      <Navbar level={"/Level-Three-Quiz"} questionnaire={"/Questionnaire_Level3"} live_generation={"/Live_Generation"} calculations={"/Calculations"}/>
       
       {/* Score Display - Only animation kept */}
       <div className="fixed top-16 left-0 z-50 px-6 py-3">
